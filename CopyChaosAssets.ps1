2b298abe52df33f7422463201714446f0c5eba05
Rupert Walker <RupertCW@gmail.com>
2023-10-05 22:43:01 +0100

    feat/fix: Swap `Write-Output` for `Write-Host`

--- CopyChaosAssets.ps1
+++ CopyChaosAssets.ps1
@@ -1,16 +1,16 @@
-Write-Output "Looking for current instances of Freelancer.exe"
+Write-Host "Looking for current instances of Freelancer.exe" -ForegroundColor Yellow
 $freelancer = Get-Process freelancer -ErrorAction SilentlyContinue
 if ($freelancer){
-Write-Output "Found $freelancer"
-Write-Output "Stopping $freelancer"
+Write-Host "Found $freelancer" -ForegroundColor Cyan
+Write-Host "Stopping $freelancer" -ForegroundColor Yellow
 Get-Process "freelancer" | Stop-Process
 $freelancer.WaitForExit()
 }
 Remove-Variable freelancer
 
-Write-Output "Copying asset files"
+Write-Host "Copying asset files" -ForegroundColor Yellow
 Copy-Item -Path ".\Assets\DATA\" -Destination "$env:LOCALAPPDATA\Freelancer HD Edition\" -Recurse -Force
 Copy-Item -Path ".\Assets\EXE\" -Destination "$env:LOCALAPPDATA\Freelancer HD Edition\" -Recurse -Force
 
-Write-Output "Starting Freelancer HD Edition in windowed mode"
+Write-Host "Starting Freelancer HD Edition in windowed mode" -ForegroundColor Green
 Start-Process -FilePath "$env:LOCALAPPDATA\Freelancer HD Edition\EXE\Freelancer.exe" -ArgumentList "-w"
