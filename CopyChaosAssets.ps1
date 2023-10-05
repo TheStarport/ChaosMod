0b13e4f9d0dad217189bedd6b46e63f812de8fcc
Rupert Walker <RupertCW@gmail.com>
2023-10-05 21:14:57 +0100

    feat: Add PowerShell script for quick copying of assets over a HD Edition installation during development

---
+++ CopyChaosAssets.ps1
@@ -0,0 +1,13 @@
+Write-Output "Looking for current instances of Freelancer.exe"
+$freelancer = Get-Process freelancer -ErrorAction SilentlyContinue
+if ($freelancer){
+Write-Output "Stopping current instances of Freelancer.exe"
+Get-Process "freelancer" | Stop-Process
+}
+Remove-Variable freelancer
+
+Write-Output "Copying asset files"
+Copy-Item -Path ".\Assets\DATA\" -Destination "$env:LOCALAPPDATA\Freelancer HD Edition\" -Recurse -Force
+
+Write-Output "Starting Freelancer HD Edition in windowed mode"
+Start-Process -FilePath "$env:LOCALAPPDATA\Freelancer HD Edition\EXE\Freelancer.exe" -ArgumentList "-w"
