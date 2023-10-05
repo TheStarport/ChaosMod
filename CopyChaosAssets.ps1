f7ad61b6741ada4a45c411c0b239f9ff6c83b5d9
Rupert Walker <RupertCW@gmail.com>
2023-10-05 21:50:12 +0100

    feat/fix: Update script to include the EXE folder

--- CopyChaosAssets.ps1
+++ CopyChaosAssets.ps1
@@ -8,6 +8,7 @@ Remove-Variable freelancer
 
 Write-Output "Copying asset files"
 Copy-Item -Path ".\Assets\DATA\" -Destination "$env:LOCALAPPDATA\Freelancer HD Edition\" -Recurse -Force
+Copy-Item -Path ".\Assets\EXE\" -Destination "$env:LOCALAPPDATA\Freelancer HD Edition\" -Recurse -Force
 
 Write-Output "Starting Freelancer HD Edition in windowed mode"
 Start-Process -FilePath "$env:LOCALAPPDATA\Freelancer HD Edition\EXE\Freelancer.exe" -ArgumentList "-w"
