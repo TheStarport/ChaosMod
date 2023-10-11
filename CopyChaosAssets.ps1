46124f1d949975e63c5beb4ba50028b330c8cf19
Rupert Walker <RupertCW@gmail.com>
2023-10-11 16:00:33 +0100

    chore: Make the copy script a bit more verbose and include a check for `FLHOOK_COPY_PATH`

--- CopyChaosAssets.ps1
+++ CopyChaosAssets.ps1
@@ -1,21 +1,27 @@
 param ($NoLaunch)
 
-Write-Host "Looking for current instances of Freelancer.exe" -ForegroundColor Yellow
+Write-Host "Looking for current instances of Freelancer.exe" -ForegroundColor Blue
 $freelancer = Get-Process freelancer -ErrorAction SilentlyContinue
-if ($freelancer){
-Write-Host "Found $freelancer" -ForegroundColor Cyan
-Write-Host "Stopping $freelancer" -ForegroundColor Yellow
-Get-Process "freelancer" | Stop-Process
-$freelancer.WaitForExit()
+if ($freelancer) {
+    Write-Host "Found $freelancer" -ForegroundColor Yellow
+    Write-Host "Stopping $freelancer" -ForegroundColor Yellow
+    Get-Process "freelancer" | Stop-Process
+    $freelancer.WaitForExit()
 }
 Remove-Variable freelancer
 
-Write-Host "Copying asset files" -ForegroundColor Yellow
-Copy-Item -Path "$PSScriptRoot\Assets\DATA\" -Destination "$env:FLHOOK_COPY_PATH\..\" -Recurse -Force
-Copy-Item -Path "$PSScriptRoot\Assets\EXE\" -Destination "$env:FLHOOK_COPY_PATH\..\" -Recurse -Force
-
-IF(!$NoLaunch)
-{
+if ($env:FLHOOK_COPY_PATH) {
+    $FullCopyDestination = Resolve-Path $env:FLHOOK_COPY_PATH\..\
+    Write-Host "Copying asset files from $PSScriptRoot to $FullCopyDestination" -ForegroundColor Blue
+    Copy-Item -Path "$PSScriptRoot\Assets\DATA\" -Destination "$FullCopyDestination" -Recurse -Force
+    Copy-Item -Path "$PSScriptRoot\Assets\EXE\" -Destination "$FullCopyDestination" -Recurse -Force
+    Remove-Variable FullCopyDestination
+}
+else {
+    Write-Host "The FLHOOK_COPY_PATH environment variable is not set! Aborting..." -ForegroundColor Red
+    exit
+}
+if (!$NoLaunch) {
     Write-Host "Starting Chaos Mod in windowed mode" -ForegroundColor Green
     Start-Process -FilePath "$env:FLHOOK_COPY_PATH\Freelancer.exe" -ArgumentList "-w"
 }
