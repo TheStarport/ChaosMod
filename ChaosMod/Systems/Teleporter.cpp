d34f887d269d4c2d9dfb5d9294f4686f229f7094
Laz <lazdisco@gmail.com>
2023-10-12 23:19:06 +0100

    fix: prevent teleporter sending you to bases that will crash the game

--- ChaosMod/Systems/Teleporter.cpp
+++ ChaosMod/Systems/Teleporter.cpp
@@ -346,21 +346,35 @@ void Teleporter::WarpToRandomSystem()
 
 void Teleporter::BeamToRandomBase(const bool inSystem)
 {
-    static std::vector bannedBases = {
-        CreateID("br_m_beryllium_miner"),  CreateID("[br_m_hydrocarbon_miner]"),
-        CreateID("[br_m_niobium_miner]"),  CreateID("[co_khc_copper_miner]"),
-        CreateID("[co_khc_cobalt_miner]"), CreateID("[co_kt_hydrocarbon_miner]"),
-        CreateID("[co_shi_h-fuel_miner]"), CreateID("[co_shi_water_miner]"),
-        CreateID("[co_ti_water_miner]"),   CreateID("[gd_gm_h-fuel_miner]"),
-        CreateID("[gd_im_oxygen_miner]"),  CreateID("[gd_im_copper_miner]"),
-        CreateID("[gd_im_silver_miner]"),  CreateID("[gd_im_water_miner]"),
-        CreateID("[rh_m_diamond_miner]"),  CreateID("intro3_base"),
-        CreateID("intro2_base"),           CreateID("intro1_base"),
-        CreateID("st03b_01_base"),         CreateID("st02_01_base"),
-        CreateID("st01_02_base"),          CreateID("iw02_03_base"),
-        CreateID("rh02_07_base"),          CreateID("li04_06_base"),
-        CreateID("li01_15_base"),
-    };
+    static std::vector bannedBases = { CreateID("[br_m_beryllium_miner]"),
+                                       CreateID("[br_m_hydrocarbon_miner]"),
+                                       CreateID("[br_m_niobium_miner]"),
+                                       CreateID("[co_khc_copper_miner]"),
+                                       CreateID("[co_khc_cobalt_miner]"),
+                                       CreateID("[co_kt_hydrocarbon_miner]"),
+                                       CreateID("[co_shi_h-fuel_miner]"),
+                                       CreateID("[co_shi_water_miner]"),
+                                       CreateID("[co_ti_water_miner]"),
+                                       CreateID("[gd_gm_h-fuel_miner]"),
+                                       CreateID("[gd_im_oxygen_miner]"),
+                                       CreateID("[gd_im_copper_miner]"),
+                                       CreateID("[gd_im_silver_miner]"),
+                                       CreateID("[gd_im_water_miner]"),
+                                       CreateID("[rh_m_diamond_miner]"),
+                                       CreateID("intro3_base"),
+                                       CreateID("intro2_base"),
+                                       CreateID("intro1_base"),
+                                       CreateID("intro4_base"),
+                                       CreateID("st03b_01_base"),
+                                       CreateID("st02_01_base"),
+                                       CreateID("st01_02_base"),
+                                       CreateID("iw02_03_base"),
+                                       CreateID("rh02_07_base"),
+                                       CreateID("li04_06_base"),
+                                       CreateID("li01_15_base"),
+                                       CreateID("bw01_05_base"),
+                                       CreateID("li01_04_base") };
+
     const auto ship = Utils::GetCShip();
     if (!ship)
     {
