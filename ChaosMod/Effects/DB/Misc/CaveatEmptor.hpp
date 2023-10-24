f3106d024a937f47a89941ca913fddc66a3ce07d
Laz <lazdisco@gmail.com>
2023-10-24 21:48:58 +0100

    fix: caveat emptor no longer crashes the game

--- ChaosMod/Effects/DB/Misc/CaveatEmptor.hpp
+++ ChaosMod/Effects/DB/Misc/CaveatEmptor.hpp
@@ -1,22 +1,63 @@
 #pragma once
 #include "Effects/ActiveEffect.hpp"
+#include "Systems/SystemComponents/GlobalTimers.hpp"
 
 class CaveatEmptor final : public ActiveEffect
 {
         typedef void(__fastcall* DestroySubObjects)(CEqObj* obj, void* edx, DamageList* list, bool b);
-        DestroySubObjects destroySubObjects = reinterpret_cast<DestroySubObjects>(0x62ABA50);
+        inline static DestroySubObjects destroySubObjects = reinterpret_cast<DestroySubObjects>(0x62ABA50);
+
+        static bool DelayedGroupExplosion(float delta)
+        {
+            CShip* ship = Utils::GetCShip();
+            if (!ship)
+            {
+                return true;
+            }
+
+            DamageList dmg;
+            const Archetype::CollisionGroup* group = ship->shiparch()->collisionGroup;
+            while (group)
+            {
+                dmg.add_damage_entry(group->id, 0.0f, static_cast<DamageEntry::SubObjFate>(2));
+                group = group->next;
+            }
+
+            destroySubObjects(ship, nullptr, &dmg, false);
+            return true;
+        }
 
         void Begin() override
         {
+            // Blow off guns that are not attached to the ship root
             CShip* ship = Utils::GetCShip();
 
-            DamageList dmg;
-            for (ushort i = 4; i < 34; i++)
+            std::vector<CAttachedEquip*> gunsToRemove;
+
+            CEquipTraverser tr(Gun);
+            CEquip* equip = GetEquipManager(ship)->Traverse(tr);
+            while (equip)
             {
-                dmg.add_damage_entry(i, 0.0f, static_cast<DamageEntry::SubObjFate>(2));
+                auto external = CAttachedEquip::cast(equip);
+                equip = GetEquipManager(ship)->Traverse(tr);
+
+                if (external->GetParentConnector(true) == ship->dunnoEngineObject1)
+                {
+                    continue;
+                }
+
+                // We are attached to the ship
+                gunsToRemove.emplace_back(external);
             }
 
-            destroySubObjects(ship, nullptr, &dmg, true);
+            for (const auto gun : gunsToRemove)
+            {
+                gun->Destroy();
+                gun->Disconnect();
+            }
+
+            // Queue wing destruction
+            GlobalTimers::i()->AddTimer(DelayedGroupExplosion, 1.0f);
         }
 
     public:
