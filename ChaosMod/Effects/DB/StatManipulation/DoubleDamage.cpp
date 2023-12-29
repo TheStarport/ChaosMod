9a80b567fed55e3f08ccea5fdc432ce11d99c70f
Laz <lazdisco@gmail.com>
2023-12-29 22:56:59 +0000

    feat/fix: implement double damage effect (#28)

--- ChaosMod/Effects/DB/StatManipulation/DoubleDamage.cpp
+++ ChaosMod/Effects/DB/StatManipulation/DoubleDamage.cpp
@@ -6,11 +6,27 @@ class DoubleDamage final : public ActiveEffect
 {
         void OnApplyDamage(uint hitSpaceObj, DamageList* dmgList, DamageEntry& dmgEntry) override
         {
-            dmgList->add_damage_entry(dmgEntry.subObj, dmgEntry.health, dmgEntry.fate);
+            const CShip* obj = dynamic_cast<CShip*>(CObject::FindFirst(CObject::CSHIP_OBJECT));
+            while (obj)
+            {
+                if (obj->id == hitSpaceObj)
+                {
+                    // Double the amount of damage that would be applied
+                    dmgEntry.health -= obj->hitPoints - dmgEntry.health;
+                    break;
+                }
+
+                obj = dynamic_cast<CShip*>(CObject::FindNext());
+            }
         }
 
     public:
         explicit DoubleDamage(const EffectInfo& effectInfo) : ActiveEffect(effectInfo) {}
 };
 
-// TODO: Implement double damage properly
+// clang-format off
+SetupEffect(DoubleDamage, {
+    .effectName = "Double Damage",
+    .description = "Wouldn't it be great if you did twice as much damage? Well now you can. NPCs might also hurt though...",
+    .category = EffectType::StatManipulation,
+});
