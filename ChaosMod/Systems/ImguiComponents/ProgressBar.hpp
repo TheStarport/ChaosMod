838938d10a5cc01c63b28bf4f8f98f2642363e30
Laz <lazdisco@gmail.com>
2024-02-25 22:24:46 +0000

    style: improve apperance of sidebars

--- ChaosMod/Systems/ImguiComponents/ProgressBar.hpp
+++ ChaosMod/Systems/ImguiComponents/ProgressBar.hpp
@@ -12,12 +12,6 @@ class ProgressBar final
         friend ImGuiManager;
         inline static float progress = 0.0f;
 
-        enum class ValueBarFlags
-        {
-            Horizontal = 0,
-            Vertical = 1 << 0,
-        };
-
         static void DrawAngledLine(const float angle, const float radius, const float lengthIn, const float lengthOut, const ImU32 color)
         {
             const float x1 = std::sinf(-angle + static_cast<float>(std::numbers::pi)) * (radius - lengthOut) + radius;
@@ -37,7 +31,6 @@ class ProgressBar final
 
             const std::chrono::hh_mm_ss hms{ floor<std::chrono::milliseconds>(tp - dp) };
 
-            const auto s = hms.seconds();
             const auto ms = hms.subseconds();
 
             // Get the draw buffer
@@ -71,33 +64,23 @@ class ProgressBar final
             DrawAngledLine(msRatio * 2.f * static_cast<float>(std::numbers::pi), outerRadius, 30.f, 0.f, colorMs);
         }
 
-        // Similar to `ImGui::ProgressBar`, but with a horizontal/vertical switch.
-        // The value text doesn't follow the value like `ImGui::ProgressBar`.
-        // Here it's simply displayed in the middle of the bar.
-        // Horizontal labels are placed to the right of the rect.
-        // Vertical labels are placed below the rect.
-        // Source: https://github.com/ocornut/imgui/issues/5263
-        static void ValueBar(const float value, const ImVec2& size, const float min = 0, const float max = 1,
-                             const ValueBarFlags flags = ValueBarFlags::Horizontal)
+        static void VerticalBar(const float value, const ImVec2& size)
         {
             using namespace ImGui;
             using namespace magic_enum::bitwise_operators;
 
-            const bool isHorizontal = !magic_enum::enum_flags_test(flags, ValueBarFlags::Vertical);
             const auto& style = GetStyle();
             const auto& drawList = GetWindowDrawList();
-            const auto& cursorPos = GetCursorScreenPos();
-            const float fraction = (value - min) / max;
-            const float frameHeight = GetFrameHeight();
-            const auto& rectSize = isHorizontal ? ImVec2{ CalcItemWidth(), frameHeight } : ImVec2{ GetFontSize() * 2, size.y };
-            const auto& rectStart = cursorPos + ImVec2{ isHorizontal ? 0 : std::max(0.0f, rectSize.x / 2), 0 };
 
-            drawList->AddRectFilled(rectStart, rectStart + rectSize, GetColorU32(ImGuiCol_FrameBg), style.FrameRounding);
-            drawList->AddRectFilled(rectStart + ImVec2{ 0, isHorizontal ? 0 : (1 - fraction) * rectSize.y },
-                                    rectStart + rectSize * ImVec2{ isHorizontal ? fraction : 1, 1 },
+            const auto& cursorPos = GetCursorScreenPos();
+            const float fraction = value / 1.f;
+
+            drawList->AddRectFilled(cursorPos, cursorPos + size, GetColorU32(ImGuiCol_FrameBg), style.FrameRounding);
+            drawList->AddRectFilled(cursorPos + ImVec2{ 0, (1 - fraction) * size.y },
+                                    cursorPos + size * ImVec2{ 1, 1 },
                                     GetColorU32(ImGuiCol_PlotHistogram),
                                     style.FrameRounding,
-                                    isHorizontal ? ImDrawFlags_RoundCornersLeft : ImDrawFlags_RoundCornersBottom);
+                                    ImDrawFlags_RoundCornersBottom);
         }
 
         static void DrawTopBar()
@@ -114,20 +97,23 @@ class ProgressBar final
         static void DrawSideBars()
         {
             const ImGuiViewport* viewport = ImGui::GetMainViewport();
-            ImGui::SetNextWindowPos({ -40.f, -10.f }, ImGuiCond_Always);
-            ImGui::SetNextWindowSize(viewport->Size + ImVec2{ 60.f, 10.f }, ImGuiCond_Always);
+            // For reasons, I cannot understand, we need to offset the window by a few pixels
+            ImGui::SetNextWindowPos({ -20.f, 0.0f }, ImGuiCond_Always);
+            ImGui::SetNextWindowSize(viewport->Size + ImVec2{ 30.f, 10.f }, ImGuiCond_Always);
             if (ImGui::Begin("ProgressWindow",
                              nullptr,
                              ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoBringToFrontOnFocus |
                                  ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoNavFocus | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoDecoration |
                                  ImGuiWindowFlags_NoTitleBar))
             {
-                ValueBar(progress, ImVec2(100, viewport->Size.y), 0.0f, 1.0f, ValueBarFlags::Vertical);
+                ImGui::SetCursorScreenPos({ 0.f, 0.f });
+                VerticalBar(progress, ImVec2(100.f, viewport->Size.y));
 
                 const auto vec = ImGui::GetWindowSize();
-                ImGui::SetCursorScreenPos(vec - ImVec2{ 140.f, vec.y });
-                ValueBar(progress, ImVec2(100.f, viewport->Size.y), 0.0f, 1.0f, ValueBarFlags::Vertical);
+                ImGui::SetCursorScreenPos(vec - ImVec2{ 120.f, vec.y });
+                VerticalBar(progress, ImVec2(100.f, viewport->Size.y));
             }
+
             ImGui::End();
         }
 
