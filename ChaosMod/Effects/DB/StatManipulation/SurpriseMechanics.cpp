8e6f470c614e71cab43134b6326e83458c198876
Laz <lazdisco@gmail.com>
2024-02-26 14:40:01 +0000

    feat: add effect to spawn in unique chaos equipment

---
+++ ChaosMod/Effects/DB/StatManipulation/SurpriseMechanics.cpp
@@ -0,0 +1,46 @@
+#include "PCH.hpp"
+
+#include "Effects/ActiveEffect.hpp"
+
+class SurpriseMechanics final : public ActiveEffect
+{
+        std::vector<uint> chaosEquipment;
+
+        void Begin() override
+        {
+            pub::Audio::PlaySoundEffect(1, CreateID("chaos_quite_ethical"));
+            pub::Player::AddCargo(1, chaosEquipment[Random::i()->Uniform(0u, chaosEquipment.size() - 1)], 1, 1.0f, false);
+        }
+
+    public:
+        explicit SurpriseMechanics(const EffectInfo& effectInfo) : ActiveEffect(effectInfo)
+        {
+            INI_Reader ini;
+            ini.open("../DATA/CHAOS/chaos_player_equipment.ini", false);
+
+            while (ini.read_header())
+            {
+                if (!ini.is_header("Gun") && !ini.is_header("Thruster") && !ini.is_header("ShieldGenerator"))
+                {
+                    continue;
+                }
+
+                while (ini.read_value())
+                {
+                    if (ini.is_value("nickname"))
+                    {
+                        chaosEquipment.emplace_back(CreateID(ini.get_value_string()));
+                    }
+                }
+            }
+        }
+};
+
+// clang-format off
+SetupEffect(SurpriseMechanics, {
+    .effectName = "Surprise Mechanics",
+    .description = "We do think the way we've implemented these kinds of mechanics is quite ethical and quite fun. They aren't gambling and we disagree that there's evidence that shows they lead to gambling",
+    .category = EffectType::StatManipulation,
+    .timingModifier = 0.0f,
+    .isTimed = false
+});
