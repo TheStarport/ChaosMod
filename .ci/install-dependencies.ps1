c3976d83d60929f49989f589add954f9ce4e4d1f
Laz <lazdisco@gmail.com>
2024-03-05 00:33:21 +0000

    1

--- .ci/install-dependencies.ps1
+++ .ci/install-dependencies.ps1
@@ -1,9 +1,11 @@
 # Explicitly install dependencies as their own step so we can cache the results
 $ErrorActionPreference = "Stop"
 $VCPKG_DIR = "$HOME\vcpkg"
+$DEST = "${HOME}/project/vcpkg_installed"
 
-Push-Location "$VCPKG_DIR"
-
-./vcpkg.exe install --x-wait-for-lock --triplet "x86-windows" --vcpkg-root "$VCPKG_DIR" --x-manifest-root="${HOME}/project" --x-install-root="${HOME}/project/vcpkg_installed/x86-windows"
-
-Pop-Location
+# Only download and install packages if the vcpkg cache is not found
+if (-not (Test-Path $DEST)) {
+    Push-Location "$VCPKG_DIR"
+    ./vcpkg.exe install --x-wait-for-lock --triplet "x86-windows" --vcpkg-root "$VCPKG_DIR" --x-manifest-root="${HOME}/project" --x-install-root="$DEST/x86-windows"
+    Pop-Location
+}
