53ebf7050e3db0f0122ee7f57349dac819b89e84
Laz <lazdisco@gmail.com>
2024-03-07 00:28:17 +0000

    feat: add patch note filtering

--- ChaosMod/Systems/ImguiComponents/PatchNotesWindow.hpp
+++ ChaosMod/Systems/ImguiComponents/PatchNotesWindow.hpp
@@ -9,9 +9,19 @@ class PatchNotesWindow final
         friend ImGuiManager;
 
         inline static bool show = false;
+        inline static char patchFilter[255];
 
         inline static DWORD* gamePauseIncrementor = PDWORD(0x667D54);
         inline static bool doDecrement = false;
+
+        static void ToLower(char* s)
+        {
+            for (char* p = s; *p; p++)
+            {
+                *p = tolower(*p);
+            }
+        }
+
         static void Render()
         {
             if (!show)
@@ -39,9 +49,29 @@ class PatchNotesWindow final
                 return;
             }
 
+            ImGui::InputText("Filter", patchFilter, sizeof(patchFilter));
+            ToLower(patchFilter);
+
             static auto& notes = PatchNotes::GetPatchNotes();
             for (auto patch = notes.rbegin(); patch != notes.rend(); ++patch)
             {
+                // If there is a filter, check if this patch contains ANY items we care about first
+                if (patchFilter[0] != '\0' && std::ranges::all_of(patch->changes,
+                                                                  [](const std::string& text)
+                                                                  {
+                                                                      char* lowerA = _strdup(text.c_str());
+                                                                      ToLower(lowerA);
+
+                                                                      const bool ret = strstr(lowerA, patchFilter) == nullptr;
+
+                                                                      free(lowerA);
+
+                                                                      return ret;
+                                                                  }))
+                {
+                    continue;
+                }
+
                 ImGui::SeparatorText(patch->version.c_str());
                 ImGui::Text(patch->date.c_str()); // NOLINT(clang-diagnostic-format-security)
                 if (!patch->releaseName.empty())
@@ -52,7 +82,14 @@ class PatchNotesWindow final
 
                 for (auto& text : patch->changes)
                 {
-                    ImGui::Text(text.c_str()); // NOLINT(clang-diagnostic-format-security)
+                    char* lowerA = _strdup(text.c_str());
+                    ToLower(lowerA);
+
+                    if (patchFilter[0] == '\0' || strstr(lowerA, patchFilter) != nullptr)
+                    {
+                        ImGui::Text(text.c_str()); // NOLINT(clang-diagnostic-format-security)
+                    }
+                    free(lowerA);
                 }
             }
 
