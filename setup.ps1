32bbcefb7262cbde368ab1572481c0562fdeec3a
Rupert Walker <RupertCW@gmail.com>
2024-04-20 21:26:44 +0100

    fix: Update setup.ps1 to work with systems that do not have WSL installed

--- setup.ps1
+++ setup.ps1
@@ -5,7 +5,7 @@ if (-not (Test-Path -Path "$PSScriptRoot/vendor/discord_game_sdk"))
     Invoke-WebRequest -Uri $url -OutFile $PSScriptRoot/discord_game_sdk.zip
 
     Write-Output "Unzipping Discord SDK"
-    unzip $PSScriptRoot/discord_game_sdk.zip -d $PSScriptRoot/vendor/discord_game_sdk
+    Expand-Archive -Path $PSScriptRoot/discord_game_sdk.zip -DestinationPath $PSScriptRoot/vendor/discord_game_sdk
 
     Write-Output "Cleaning Up"
     Remove-Item $PSScriptRoot/discord_game_sdk.zip
