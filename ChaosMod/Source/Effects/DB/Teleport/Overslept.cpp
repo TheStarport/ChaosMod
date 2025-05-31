f2fa319e44b4686deb0b2c8f6b089bcbe5b2a03d
Laz <lazdisco@gmail.com>
2025-05-31 13:11:18 +0100

    feat: add sound to the overslept effect #137

--- ChaosMod/Source/Effects/DB/Teleport/Overslept.cpp
+++ ChaosMod/Source/Effects/DB/Teleport/Overslept.cpp
@@ -1,12 +1,16 @@
 #include "PCH.hpp"
 
-#include "Effects/ActiveEffect.hpp"
 #include "Components/Teleporter.hpp"
+#include "Effects/ActiveEffect.hpp"
 
 class Overslept final : public ActiveEffect
 {
         // To Omicron Alpha you go!
-        void Begin() override { Get<Teleporter>()->WarpToPoint(CreateID("hi01"), { -7480.f, 67.f, 26175.f }, { -30.f, 8.f, 0.f }); }
+        void Begin() override
+        {
+            Get<Teleporter>()->WarpToPoint(CreateID("hi01"), { -7480.f, 67.f, 26175.f }, { -30.f, 8.f, 0.f });
+            pub::Audio::PlaySoundEffect(1, CreateID("chaos_finally_awake"));
+        }
 
     public:
         explicit Overslept(const EffectInfo& effectInfo) : ActiveEffect(effectInfo) {}
